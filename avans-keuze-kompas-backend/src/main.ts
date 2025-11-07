64f9f47b5a84f82943b37ee6245a42d16f55eb91
Its A Wild Syneon <jari@jarivankaam.nl>
2025-11-07 16:35:46 +0100

    Update CORS origin to remove port number

--- avans-keuze-kompas-backend/src/main.ts
+++ avans-keuze-kompas-backend/src/main.ts
@@ -4,7 +4,7 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({ origin: 'http://akk-frontend.panel.evonix-development.tech:3000', credentials: true });
+  app.enableCors({ origin: 'http://akk-frontend.panel.evonix-development.tech', credentials: true });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(4000);
 }
