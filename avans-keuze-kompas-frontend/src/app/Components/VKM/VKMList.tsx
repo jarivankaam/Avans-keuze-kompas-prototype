a3a7a956020687d7129c82fb9de2eb334c287f34
Its A Wild Syneon <jari@jarivankaam.nl>
2025-11-07 15:50:25 +0100

    Convert _id to string before deletion

--- avans-keuze-kompas-frontend/src/app/Components/VKM/VKMList.tsx
+++ avans-keuze-kompas-frontend/src/app/Components/VKM/VKMList.tsx
@@ -22,7 +22,7 @@ export default function VKMList() {
 
   async function handleDelete(_id: number | string) {
     if (!confirm("Are you sure you want to delete this VKM?")) return;
-    await deleteItem(_id);
+    await deleteItem(String(_id));
     load();
   }
 
