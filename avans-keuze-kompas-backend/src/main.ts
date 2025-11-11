80425e8143625b329339d2c779015473b0b06251
jarivankaam <jari@jarivankaam.nl>
2025-11-11 14:42:05 +0100

    added factories and singletons

--- avans-keuze-kompas-backend/src/main.ts
+++ avans-keuze-kompas-backend/src/main.ts
@@ -7,13 +7,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   // Enable cookie parser (required for reading cookies)
-  app.use(cookieParser());
+  app.use(cookieParser);
 
   // Enable CORS with credentials support
   app.enableCors({
     origin: 'http://akk-frontend.panel.evonix-development.tech',
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
     exposedHeaders: ['Set-Cookie'],
   });
