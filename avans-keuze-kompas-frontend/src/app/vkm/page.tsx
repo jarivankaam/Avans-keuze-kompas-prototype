e9952b708b1c4b98127156c7ce0bd70804ce3577
jarivankaam <jari@jarivankaam.nl>
2025-11-11 15:07:10 +0100

    fixed vkm page

--- avans-keuze-kompas-frontend/src/app/vkm/page.tsx
+++ avans-keuze-kompas-frontend/src/app/vkm/page.tsx
@@ -3,14 +3,18 @@
 import VKMList from "../Components/VKM/VKMList";
 import { useAuth } from "../lib/auth/useAuth";
 import LoginForm from "../Components/Auth/loginForm";
+import { Header } from "../Components/layout/header/Header";
 
 export default function HomePage() {
   const { isLoggedIn } = useAuth();
 
   return (
-    <main className="p-6 max-w-4xl mx-auto">
-      <h1 className="text-3xl font-bold mb-6">VKMs Dashboard</h1>
-      {isLoggedIn ? <VKMList /> : <LoginForm />}
-    </main>
+    <>
+      <Header />
+      <main className="p-6 max-w-4xl mx-auto">
+        <h1 className="text-3xl font-bold mb-6">VKMs Dashboard</h1>
+        {isLoggedIn ? <VKMList /> : <LoginForm />}
+      </main>
+    </>
   );
 }
