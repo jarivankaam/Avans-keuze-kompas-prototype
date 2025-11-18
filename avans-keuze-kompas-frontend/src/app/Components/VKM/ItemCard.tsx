a1e623ad8ced31f289095d90ffcb404c5be25b68
jarivankaam <jari@jarivankaam.nl>
2025-11-18 15:54:01 +0100

    also changes cards quickly

--- avans-keuze-kompas-frontend/src/app/Components/VKM/ItemCard.tsx
+++ avans-keuze-kompas-frontend/src/app/Components/VKM/ItemCard.tsx
@@ -2,11 +2,14 @@
 import {Item} from '../../lib/useItem';
 type Props = {
     item: Item,
+    index: number,
 }
 
-export const ItemCard = ({ item }: Props)=>{
+export const ItemCard = ({ item, index }: Props)=>{
+    const bgColor = index % 2 === 0 ? '#F8F8F8' : '#FF8B8B';
+
     return (
-        <div className="p-4 rounded shadow bg-gray-50">
+        <div className="p-4 rounded shadow" style={{ backgroundColor: bgColor, width: '400px', height: '350px' }}>
             <h2 className="text-lg font-semibold">{item.name}</h2>
             <p className="text-gray-600 truncate">{item.description}</p>
         </div>
--- avans-keuze-kompas-frontend/src/app/Components/VKM/VKMItem.tsx
+++ avans-keuze-kompas-frontend/src/app/Components/VKM/VKMItem.tsx
@@ -14,8 +14,8 @@ export default function ItemsView() {
             <h2 className="text-3xl mb-2 font-bold">Alle keuze modules:</h2>
             <div className="grid grid-cols-1 lg:grid-cols-3 md:grid-cols-3 sm:grid-cols-3 gap-4">
 
-                {items.map(item => (
-                    <ItemCard key={item._id} item={item} />
+                {items.map((item, index) => (
+                    <ItemCard key={item._id} item={item} index={index} />
                 ))}
             </div>
         </div>
